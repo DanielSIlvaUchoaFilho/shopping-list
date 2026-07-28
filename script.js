82c54579059945318467df17869cb463c667d09a
Daniel <aplicativos137137137@gmail.com>
2026-07-28 12:34:49 -0300

    Update & Reset State

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ function onAddItemSubmit(e) {
         return;
     }
 
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector(".edit-mode");
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove("edit-mode");
+        itemToEdit.remove();
+        isEditMode = false;
+    }
+
     // Create item DOM element
     addItemToDOM(newItem);
 
@@ -150,6 +160,8 @@ function filterItems(e) {
 }
 
 function checkUi() {
+    itemInput.value = "";
+
     const items = itemList.querySelectorAll("li");
     if (items.length === 0) {
         clearBtn.style.display = "none";
@@ -158,6 +170,9 @@ function checkUi() {
         clearBtn.style.display = "block";
         itemFilter.style.display = "block";
     }
+
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    formBtn.style.backgroundColor = "#333";
 }
 
 // initialize app
